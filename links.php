db77eee1871e33566d38c06d31d6fefc05e5b908
tom <88882@glr.nl>
2023-09-15 13:28:15 +0200

    file create

---
+++ links.php
@@ -0,0 +1,5 @@
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;1,100;1,200;1,300;1,600&display=swap" rel="stylesheet">
+
+<link rel="icon" type="image/x-icon" href="./assets/media/geode.png">
